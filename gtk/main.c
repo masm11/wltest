2ba29f5245a1c473c0222f8f758fba787b7757b4
Yuuki Harano <masm@masm11.ddo.jp>
2017-05-13 21:28:59 +0900

    エラー出ずにウィンドウには何かが描画されている。

--- gtk/main.c
+++ gtk/main.c
@@ -12,10 +12,238 @@
 
 static GtkWidget *drawable;
 
-static EGLDisplay dpy = 0;
-static EGLSurface sfc;
-static EGLContext ctxt;
-static int shader_program;
+
+static const char *srcVertexShader =
+	"attribute vec4 position0;\n"
+	"attribute vec3 normal0;\n"
+	"varying vec4 vsout_color0;\n"
+	"uniform vec4 matPVW[4];\n"
+	"void main() {\n"
+	"vec4 pos;\n"
+	"  pos = matPVW[0] * position0.xxxx;\n"
+	"  pos += matPVW[1]* position0.yyyy;\n"
+	"  pos += matPVW[2]* position0.zzzz;\n"
+	"  pos += matPVW[3]* position0.wwww;\n"
+	"  gl_Position = pos;\n"
+	"  float lmb = clamp( dot( vec3(0.0, 0.5, 0.5), normalize(normal0.xyz)), 0.f, 1.f );\n"
+	"  lmb = lmb * 0.5 + 0.5;\n"
+	"  vsout_color0.rgb = vec3(lmb,lmb,lmb);\n"
+	"  vsout_color0.a = 1.0;\n"
+	"}";
+
+static const char *srcFragmentShader =
+	// "precision mediump float;\n"
+	"varying vec4 vsout_color0;\n"
+	"void main() {\n"
+	"  gl_FragColor = vsout_color0;\n"
+	"}";
+
+static void checkCompiled(int shader)
+{
+    int status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status != GL_TRUE) {
+	GLint length;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+	if (length) {
+	    char *buf = malloc(length);
+	    glGetShaderInfoLog(shader, length, NULL, buf);
+	    fprintf(stderr, "CompileLog: %s\n", buf);
+	}
+	exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "Compile Succeed.\n");
+}
+
+static int createShaderProgram(const char *srcVS, const char *srcFS)
+{
+    int shaderVS = glCreateShader(GL_VERTEX_SHADER);
+    int shaderFS = glCreateShader(GL_FRAGMENT_SHADER);
+    
+    fprintf(stderr, "vertex shader.\n");
+    glShaderSource( shaderVS, 1, &srcVS, 0 );
+    int errCode = glGetError();
+    if( errCode != GL_NO_ERROR ) {
+	fprintf( stderr, "GLErr.  %X\n", errCode );
+	exit(1);
+    }
+    
+    glCompileShader( shaderVS );
+    checkCompiled( shaderVS );
+    
+    fprintf(stderr, "fragment shader.\n");
+    glShaderSource( shaderFS, 1, &srcFS, NULL );
+    glCompileShader( shaderFS );
+    checkCompiled( shaderFS );
+    
+    int program = glCreateProgram();
+    glAttachShader( program, shaderVS );
+    glAttachShader( program, shaderFS );
+    
+    glLinkProgram( program );
+    
+    return program;
+}
+
+struct VertexPosition {
+    float x, y, z;
+};
+struct VertexNormal {
+    float nx, ny, nz;
+};
+
+struct VertexPN {
+    struct VertexPosition Position;
+    struct VertexNormal Normal;
+};
+
+static const double PI = 3.14159265;
+
+#define TORUS_N 20
+
+static void create_torus(uint16_t *indices, struct VertexPN *vertices)
+{
+    float radius = 3.0f;
+    float minorRadius = 1.0f;
+    int n = TORUS_N;
+    
+    int vertexCount = (n+1) * (n+1);
+    
+    int idx = 0;
+    for( int i = 0; i <= n; ++i ) {
+	double ph = PI * 2.0 * i / n;
+	double r = cos( ph ) * minorRadius;
+	double y = sin( ph ) * minorRadius;
+	
+	for( int j = 0; j <= n; ++j ) {
+	    double th = 2.0 * PI * j / n;
+	    float x = (r + radius) * cos(th);
+	    float z = (r + radius) * sin(th);
+	    
+	    struct VertexPN v;
+	    v.Position.x = x;
+	    v.Position.y = (float)y;
+	    v.Position.z = z;
+	    
+	    float nx = (float)( r * cos(th) );
+	    float ny = (float) y;
+	    float nz = (float)( r * sin(th) );
+	    v.Normal.nx = nx;
+	    v.Normal.ny = ny;
+	    v.Normal.nz = nz;
+	    
+	    vertices[idx++] = v;
+	}
+    }
+    
+    idx = 0;
+    for( int i = 0; i < n; ++i ) {
+	for( int j = 0; j < n; ++j ) {
+	    int index = (n+1) * j + i;
+	    indices[idx++] = index;
+	    indices[idx++] = index + n + 2;
+	    indices[idx++] = index + 1;
+	    
+	    indices[idx++] = index;
+	    indices[idx++] = index + n + 1;
+	    indices[idx++] = index + n + 2;
+	}
+    }
+}
+
+struct DrawBatch {
+  GLuint vb, ib;
+  GLuint shader;
+  int indexCount;
+} drawObj;
+
+GLint locPVW;
+
+static void CreateResource(void)
+{
+    drawObj.shader = createShaderProgram( srcVertexShader, srcFragmentShader );
+    GLint locPos = glGetAttribLocation( drawObj.shader, "position0" );
+    GLint locNrm = glGetAttribLocation( drawObj.shader, "normal0" );
+    
+    locPVW = glGetUniformLocation( drawObj.shader, "matPVW" );
+    
+    uint16_t indices_torus[TORUS_N * TORUS_N * 6];
+    struct VertexPN vertices_torus[(TORUS_N + 1) * (TORUS_N + 1)];
+    create_torus(indices_torus, vertices_torus);
+    glGenBuffers(1, &drawObj.vb);
+    glBindBuffer(GL_ARRAY_BUFFER, drawObj.vb);
+    glBufferData(GL_ARRAY_BUFFER, sizeof vertices_torus, vertices_torus, GL_STATIC_DRAW);
+    glGenBuffers(1, &drawObj.ib);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, drawObj.ib);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices_torus, indices_torus, GL_STATIC_DRAW);
+    drawObj.indexCount = TORUS_N * TORUS_N * 6;
+    
+    char* offset = NULL;
+    int stride = sizeof(struct VertexPN);
+    glVertexAttribPointer( locPos, 3, GL_FLOAT, GL_FALSE, stride, offset );
+    offset += sizeof(struct VertexPosition);
+    glVertexAttribPointer( locNrm, 3, GL_FLOAT, GL_FALSE, stride, offset );
+    offset += sizeof(struct VertexNormal);
+    
+    glEnableVertexAttribArray( locPos );
+    glEnableVertexAttribArray( locNrm );
+}
+
+static void DestroyResource(void)
+{
+    glDeleteBuffers(1, &drawObj.vb);
+    glDeleteBuffers(1, &drawObj.ib);
+    glDeleteProgram(drawObj.shader);
+}
+
+struct vec3 {
+    float v[3];
+};
+
+struct mat4 {
+    float v[4][4];
+};
+
+static void drawCube(int width, int height)
+{
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    static double angle = 0.0f;
+    angle += .01f;
+    if( angle > 3600.0 ) {
+	angle -= 3600.0;
+    }
+    
+#if 0
+    struct vec3 cameraPos = {
+	{ 0.0, 0.0f, 10.0f },
+    };
+    glm::mat4 proj = glm::perspective<float>( 30.0f, float(width)/float(height), 1.0f, 100.0f );
+    glm::mat4 view = glm::lookAt<float>( cameraPos, glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,1.0f,0.0f) );
+    glm::mat4 world= glm::rotate( glm::mat4(1.0f), (float)angle, glm::vec3(0.0f,0.0f,1.0f) );
+    world= glm::rotate( world, (float) angle * 0.5f, glm::vec3( 0.0f, 0.0f, 1.0f ) );
+    world= glm::rotate( world, (float) angle * 0.5f, glm::vec3( 1.0f, 0.0f, 0.0f ));
+#endif
+    glUseProgram( drawObj.shader );
+    
+    // glm::mat4 pvw = proj * view * world;
+    struct mat4 pvw = {
+	{
+	    { 1, 0, 0, 0 },
+	    { 0, 1, 0, 0 },
+	    { 0, 0, 1, -10 },
+	    { 0, 0, 0, 1 },
+	},
+    };
+    glUniform4fv( locPVW, 4, (float *) &pvw );
+    
+    glBindBuffer( GL_ARRAY_BUFFER, drawObj.vb );
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, drawObj.ib );
+    
+    glDrawElements( GL_TRIANGLES, drawObj.indexCount, GL_UNSIGNED_SHORT, NULL );   
+}
+
 
 #define CHECK_GL_ERROR() check_gl_error(__FILE__, __LINE__)
 static void check_gl_error(const char *file, int lineno)
@@ -37,317 +265,30 @@ static void check_egl_error(const char *file, int lineno)
     exit(1);
 }
 
-static void init_shaders(void)
+static int create_texture(void)
 {
-    const GLchar *vertex_shader_source = 
-	    "attribute mediump vec4 attr_pos;"
-	    "void main() {"
-	    "    gl_Position = attr_pos;"
-	    "}";
-    // シェーダオブジェクトの作成
-    int vert_shader = glCreateShader(GL_VERTEX_SHADER);
-    CHECK_GL_ERROR();
-    assert(vert_shader != 0);
-    
-    glShaderSource(vert_shader, 1, &vertex_shader_source, NULL);
-    glCompileShader(vert_shader);
-    
-    // コンパイルエラーをチェックする
-    {
-	GLint compileSuccess = 0;
-	glGetShaderiv(vert_shader, GL_COMPILE_STATUS, &compileSuccess);
-	if (compileSuccess == GL_FALSE) {
-	    // エラーが発生した
-	    GLint infoLen = 0;
-	    // エラーメッセージを取得
-	    glGetShaderiv(vert_shader, GL_INFO_LOG_LENGTH, &infoLen);
-	    if (infoLen > 1) {
-		GLchar *message = (GLchar*) calloc(infoLen, sizeof(GLchar));
-		glGetShaderInfoLog(vert_shader, infoLen, NULL, message);
-		printf("%s\n", message);
-		free((void*) message);
-	    } else {
-		printf("comple error not info...\n");
-	    }
+    static unsigned char data[16 * 16 * 4];
+    int tex;
+
+    for (int y = 0; y < 16; y++) {
+	for (int x = 0; x < 16; x++) {
+	    data[(y * 16 + x) * 4 + 0] = rand();
+	    data[(y * 16 + x) * 4 + 1] = rand();
+	    data[(y * 16 + x) * 4 + 2] = rand();
+	    data[(y * 16 + x) * 4 + 3] = rand();
 	}
-	assert(compileSuccess == GL_TRUE);
     }
-    
-    const GLchar *fragment_shader_source = 
-	    "void main() {"
-	    "    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);"
-	    "}";
-    // シェーダーオブジェクトを作成する
-    int frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    assert(frag_shader != 0);
-    glShaderSource(frag_shader, 1, &fragment_shader_source, NULL);
-    glCompileShader(frag_shader);
-    
-    // コンパイルエラーをチェックする
-    {
-	GLint compileSuccess = 0;
-	glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &compileSuccess);
-	if (compileSuccess == GL_FALSE) {
-	    // エラーが発生した
-	    GLint infoLen = 0;
-	    // エラーメッセージを取得
-	    glGetShaderiv(frag_shader, GL_INFO_LOG_LENGTH, &infoLen);
-	    if (infoLen > 1) {
-		GLchar *message = (GLchar*) calloc(infoLen, sizeof(GLchar));
-		glGetShaderInfoLog(frag_shader, infoLen, NULL, message);
-		printf("%s\n", message);
-		free((void*) message);
-	    } else {
-		printf("comple error not info...\n");
-	    }
-	}
-	assert(compileSuccess == GL_TRUE);
-    }
-    
-    shader_program = glCreateProgram();
-    assert(shader_program != 0);
-    
-    // 頂点シェーダーとプログラムを関連付ける
-    glAttachShader(shader_program, vert_shader);
-    assert(glGetError() == GL_NO_ERROR);
-    // フラグメントシェーダーとプログラムを関連付ける
-    glAttachShader(shader_program, frag_shader);
-    assert(glGetError() == GL_NO_ERROR);
-    
-    // リンク処理を行う
-    glLinkProgram(shader_program);
-    
-    // リンクエラーをチェックする
-    {
-	GLint linkSuccess = 0;
-	glGetProgramiv(shader_program, GL_LINK_STATUS, &linkSuccess);
-	
-	if (linkSuccess == GL_FALSE) {
-	    // エラーが発生したため、状態をチェックする
-	    GLint infoLen = 0;
-	    // エラーメッセージを取得
-	    glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &infoLen);
-	    if (infoLen > 1) {
-		GLchar *message = (GLchar*) calloc(infoLen, sizeof(GLchar));
-		glGetProgramInfoLog(shader_program, infoLen, NULL, message);
-		printf("%s\n", message);
-		free((void*) message);
-	    }
-	}
-	
-	// GL_NO_ERRORであることを検証する
-	assert(linkSuccess == GL_TRUE);
-    }
-    
-    int attr_pos = glGetAttribLocation(shader_program, "attr_pos");
-    assert(attr_pos >= 0);
-    
-    glUseProgram(shader_program);
-}
 
-static void init_egl(void)
-{
-    struct wl_display *native_dpy
-	    = gdk_wayland_display_get_wl_display(
-		    gdk_window_get_display(
-			    gtk_widget_get_window(drawable)
-			));
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    // glEnable(GL_SCISSOR_TEST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 16, 16, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     
-    dpy = eglGetDisplay(native_dpy);
-    if (dpy == EGL_NO_DISPLAY) {
-	printf("eglGetDisplay() failed.\n");
-	exit(1);
-    }
-    CHECK_EGL_ERROR();
-    
-    int major, minor;
-    if (!eglInitialize(dpy, &major, &minor)) {
-	CHECK_EGL_ERROR();
-	printf("eglInitialize() failed.\n");
-	exit(1);
-    }
-    printf("%d.%d\n", major, minor);
-    
-    int attribs[] = {
-	EGL_RED_SIZE,		8,
-	EGL_GREEN_SIZE,		8,
-	EGL_BLUE_SIZE,		8,
-	EGL_ALPHA_SIZE,		8,
-	EGL_DEPTH_SIZE,		16,
-	EGL_SURFACE_TYPE,	EGL_WINDOW_BIT,
-	EGL_RENDERABLE_TYPE,	EGL_OPENGL_ES2_BIT,
-	EGL_NONE,
-    };
-    EGLConfig config;
-    int num_configs = -1;
-    if (!eglChooseConfig(dpy, attribs, &config, 1, &num_configs)) {
-	CHECK_EGL_ERROR();
-	printf("eglChooseConfig() failed.\n");
-	exit(1);
-    }
-    printf("num_configs=%d.\n", num_configs);
-    int v = -1;
-    if (!eglGetConfigAttrib(dpy, config, EGL_BUFFER_SIZE, &v)) {
-	CHECK_EGL_ERROR();
-	printf("eglGetConfigAttrib(buffer_size) failed.\n");
-	exit(1);
-    }
-    printf("buffer_size=%d.\n", v);
-    v = -1;
-    if (!eglGetConfigAttrib(dpy, config, EGL_DEPTH_SIZE, &v)) {
-	CHECK_EGL_ERROR();
-	printf("eglGetConfigAttrib(depth_size) failed.\n");
-	exit(1);
-    }
-    printf("depth_size=%d.\n", v);
-    
-    int context_attrib[] = {
-	EGL_CONTEXT_CLIENT_VERSION, 2,
-	EGL_NONE,
-    };
-    ctxt = eglCreateContext(dpy, config, EGL_NO_CONTEXT, context_attrib);
-    if (ctxt == EGL_NO_CONTEXT) {
-	CHECK_EGL_ERROR();
-	printf("eglCreateContext() failed.\n");
-	exit(1);
-    }
-    
-    struct wl_surface *native_window =
-	    gdk_wayland_window_get_wl_surface(gtk_widget_get_window(drawable));
-    printf("native_window=%p\n", native_window);
-    struct wl_egl_window *win = wl_egl_window_create(native_window, 400, 400);
-    printf("win=%p\n", win);
-    sfc = eglCreateWindowSurface(dpy, config, win, NULL);
-    if (sfc == EGL_NO_SURFACE) {
-	CHECK_EGL_ERROR();
-	printf("eglCreateWindowSurface() failed.\n");
-	exit(1);
-    }
-    
-    eglMakeCurrent(dpy, sfc, sfc, ctxt);
-    
-    init_shaders();
-}
-
-#if 0
-typedef struct { float v[3]; } vec3;
-typedef struct { float v[4][4]; } mat4;
-
-static mat4 rotate(mat4 cur, float angle, vec3 axis)
-{
-    return cur;
-}
-
-#if 0
-template<typename T>
-GLuint createBufferObject( std::vector<T>& src, GLuint type ){
-    GLuint vbo;
-    glGenBuffers( 1, &vbo );
-    glBindBuffer( type, vbo );
-    glBufferData( type, sizeof(T) * src.size(), src.data(), GL_STATIC_DRAW );
-    return vbo;
-}
-
-static void CreateResource() {
-    drawObj.shader = createShaderProgram( srcVertexShader, srcFragmentShader );
-    GLint locPos = glGetAttribLocation( drawObj.shader, "position0" );
-    GLint locNrm = glGetAttribLocation( drawObj.shader, "normal0" );
-    
-    locPVW = glGetUniformLocation( drawObj.shader, "matPVW" );
-    
-    std::vector<uint16_t> indicesTorus;
-    std::vector<VertexPN> verticesTorus;
-    createTorus( indicesTorus, verticesTorus );
-    drawObj.vb = createBufferObject( verticesTorus, GL_ARRAY_BUFFER );
-    drawObj.ib = createBufferObject( indicesTorus, GL_ELEMENT_ARRAY_BUFFER );
-    drawObj.indexCount = indicesTorus.size();
-    
-    char* offset = NULL;
-    int stride = sizeof(VertexPN);
-    glVertexAttribPointer( locPos, 3, GL_FLOAT, GL_FALSE, stride, offset );
-    offset += sizeof(VertexPosition);
-    glVertexAttribPointer( locNrm, 3, GL_FLOAT, GL_FALSE, stride, offset );
-    offset += sizeof(VertexNormal);
-    
-    glEnableVertexAttribArray( locPos );
-    glEnableVertexAttribArray( locNrm );
-}
-#endif
-
-static void draw_cube(void)
-{
-    glEnable(GL_DEPTH_TEST);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    static double angle = 0.0f;
-    angle += .01f;
-    if (angle > 3600.0)
-	angle -= 3600.0;
-    
-    vec3 cameraPos = { { 0.0, 0.0f, 10.0f } };
-    // mat4 proj = glm::perspective<float>(30.0f, (float) width / (float) height, 1.0f, 100.0f);
-    // glm::mat4 view = glm::lookAt<float>(cameraPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    mat4 world = { { { 1, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } } };
-    vec3 axis_x = { { 1, 0, 0 } };
-    vec3 axis_y = { { 0, 1, 0 } };
-    vec3 axis_z = { { 0, 0, 1 } };
-    world = rotate(world, (float) angle, axis_y);
-    world = rotate(world, (float) angle * 0.5f, axis_z);
-    world = rotate(world, (float) angle * 0.5f, axis_x);
-    glUseProgram(shader_program);
-    
-    mat4 pvw = world;
-    glUniform4fv(locPVW, 4, pvw.v[0]);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, drawObj.vb);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, drawObj.ib);
-    
-    glDrawElements(GL_TRIANGLES, drawObj.indexCount, GL_UNSIGNED_SHORT, NULL);
-}
-#endif
-
-static void draw(void)
-{
-    if (dpy == 0)
-	init_egl();
-    
-/*
-    cairo_device_t *device = cairo_egl_device_create(dpy, ctxt);
-    cairo_surface_t *surface = cairo_gl_surface_create_for_egl(device, sfc, 500, 500);
-    cairo_t *cr = cairo_create(surface);
-*/
-    
-    glClearColor(0, 0, 0, 1);
-    CHECK_GL_ERROR();
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    CHECK_GL_ERROR();
-    // glViewport(0, 0, 400, 400);
-    CHECK_GL_ERROR();
-    
-    eglSwapBuffers(dpy, sfc);
-    CHECK_GL_ERROR();
-    printf("%ld: draw, ", time(NULL));
-    fflush(stdout);
-    
-/*
-    cairo_destroy(cr);
-    cairo_surface_destroy(surface);
-    cairo_device_destroy(device);
-*/
-}
-
-static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer user_data)
-{
-/*
-    cairo_set_source_rgb(cr, 0, 0, 0);
-    cairo_select_font_face(cr, "sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_set_font_size(cr, 40.0);
-    
-    cairo_move_to(cr, 10.0, 50.0);
-    cairo_show_text(cr, "test");
-*/
-    draw();
-    return FALSE;
+    return tex;
 }
 
 static gboolean timeout_cb(gpointer user_data)
@@ -358,11 +299,21 @@ static gboolean timeout_cb(gpointer user_data)
 
 static gboolean render(GtkGLArea *area, GdkGLContext *context)
 {
+    static int tex = 0;
+    if (tex == 0) {
+	tex = create_texture();
+	CreateResource();
+    }
+    
     static float v = 0.0f;
     glClearColor(v, v, v, 1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     if ((v += 0.01f) >= 1.0f)
 	v -= 1.0f;
+    
+    drawCube(400, 400);
+    
+    CHECK_GL_ERROR();
     
     return TRUE;
 }
